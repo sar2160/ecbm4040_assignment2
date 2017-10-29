cfcf58b8f3a3062df81842b3dce3cfaee633aca6
sar2160 <srimmele@gmail.com>
2017-10-29 19:14:00 -0400

    finish task 1

---
+++ ecbm4040/neuralnets/cnn.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env/ python
+# ECBM E4040 Fall 2017 Assignment 2
+# TensorFlow CNN
+
+import tensorflow as tf
+import numpy as np
+import time
+
+####################################
+# TODO: Build your own LeNet model #
+####################################
+
+
+def my_LeNet(input_x, input_y):
+    raise NotImplementedError
+
+####################################
+#        End of your code          #
+####################################
+
+##########################################
+# TODO: Build your own training function #
+##########################################
+
+
+def my_training(X_train, y_train, X_val, y_val):
+
+    raise NotImplementedError
+##########################################
+#            End of your code            #
+##########################################
+
+
+def my_training_task4():
+    # TODO: Copy my_training function, make modifications so that it uses your
+    # data generator from task 4 to train.
+    raise NotImplementedError
---
+++ ecbm4040/neuralnets/kaggle.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# ECBM E4040 Fall 2017 Assignment 2
+# This script is intended for task 5 Kaggle competition. Use it however you want.
